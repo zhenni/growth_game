202dad734194533ee2dfc16d8ff617cb68b279bb
zhenni <94haelnut@gmail.com>
2016-07-16 14:29:35 +0800

    Add Player and Card class

---
+++ backend/card/Card.java
@@ -0,0 +1,14 @@
+package card;
+
+import java.util.*;
+
+import player.*;
+
+public class Card {
+    protected Player cardFromPlayer;
+    protected
+
+    public Card(cardFromPlayer){
+        this.cardFromPlayer = cardFromPlayer;
+    }
+}
