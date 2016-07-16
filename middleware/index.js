021ded6fba5fc3bde9cd5a3a65dcf2b3e471fa2b
zhenni <94hazelnut@gmail.com>
2016-07-16 21:55:58 +0800

    增加中间件模型

---
+++ middleware/index.js
@@ -0,0 +1,26 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var websocketServer = require('ws').Server;
+var wss = new websocketServer({ port: 8888 });
+
+wss.on('connection', function(ws) {
+  ws.on('message', function(message) {
+    console.log('received: %s', message);
+  });
+  ws.send('something');
+})
+
+server.listen(3000);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  console.log('hello');
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
