5121074ed5cb9c9f70d0932530b5f99ca45f73b9
zhenni <94haelnut@gmail.com>
2016-07-16 15:16:25 +0800

    Add Cards of Different Types

--- backend/card/Card.java
+++ backend/card/Card.java
@@ -5,10 +5,50 @@ import java.util.*;
 import player.*;
 
 public class Card {
-    protected Player cardFromPlayer;
-    protected
+    protected static int cardNum = 0;
 
-    public Card(cardFromPlayer){
-        this.cardFromPlayer = cardFromPlayer;
+    protected final int ID;
+    protected double prob = 0;
+
+    protected int hp; // Decrease hp value of the player
+    protected int[] deltaAdd = new int[3];  // [0] for develop, [1] for hp, [2] for gp
+    protected int[] deltaTimes = new int[3];
+
+    protected int cardToPlayer; // -1 for all, o.w. for the selected player
+
+    public Card(){
+        ID = cardNum++;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public double getProb() {
+        return prob;
+    }
+
+    public void setProb(double prob) {
+        this.prob = prob;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public int getCardToPlayer() {
+        return cardToPlayer;
+    }
+
+    public int[] getDeltaAdd() {
+        return deltaAdd;
+    }
+
+    public int[] getDeltaTimes() {
+        return deltaTimes;
+    }
+
+    public String toString(){
+        return "Card";
     }
 }
---
+++ backend/card/Disable.java
@@ -0,0 +1,12 @@
+package card;
+
+import java.util.*;
+
+import player.*;
+
+public class Disable extends Card {
+    @Override
+    public String toString(){
+        return "Disable";
+    }
+}
