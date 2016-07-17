6a37fea93215aa5e4891c04a9e996332605cec9f
SkyAo <csvwolf@qq.com>
2016-07-17 14:35:14 +0800

    更新目前的进度

--- front-end/src/app.vue
+++ front-end/src/app.vue
@@ -2,14 +2,14 @@
   <!-- <h1>front-end</h1>
   <p>A vue project.</p> -->
   <!-- <avatar-list :self="0"></avatar-list> -->
-  <div class="layout-total" :load="emitInit()">
+  <div class="layout-total">
     <div class="layout-mainPlayArea">
       <div class="layout-otherPlayer">
         <avatar-list :self="0" :my-avatar="false"></card-list>
       </div>
       <div class="layout-battleArea">
-        <div class="layout-stageArea">
-          {{stage}}
+        <div class="layout-stageArea" :load="emitInit()">
+          {{stage}} {{playerId}}
         </div>
         <div class="layout-linkArea">
           <card-list></card-list>
@@ -21,7 +21,7 @@
         <avatar-list :self="0" :my-avatar="true"></card-list>
       </div>
       <div class="layout-cardArea">
-        <card-list></card-list>
+        <card-list :need-use-card="needUseCard" :></card-list>
       </div>
     </div>
   </div>
@@ -46,15 +46,39 @@
       return {
         socket: io('http://localhost:3000/game-room'),
         stage: '等待开始游戏',
-        playerId: 3,
-        roundCounter: 0,  // 回合数
-        timesCounter: 0 // 轮数
+        playerId: null,
+        stageList: [
+          '等待开始游戏',
+          '抽牌阶段',
+          '发牌阶段',
+          '养成阶段',
+          '连锁阶段',
+          '清算阶段'
+        ],
+        allCard: [
+
+        ],
+        cardList: [
+
+        ],
+        hp: [],
+        gp: [],
+        yourturn: false,
+        needUseCard: false,
+        needSetPerson: false,
+        nextPlayer: null,
+        nextOperator: null,
+        clearDesk: false,
+        desk: []
       };
     },
     computed: {
 
     },
     methods: {
+      giveUp: function() {
+        return -1;
+      },
       uploadStage: function(stage) {
         this.stage = stage;
       },
@@ -63,21 +87,84 @@
       },
       emitInit: function() {
         // this.socket = ;
-        console.log('hello13');
+        // console.log('hello13');
         // this.socket.manager('connect', (data) => {
         //   this.uploadStage('开始游戏');
         //
         // })
         // socket.emit('hello', message);
-        this.socket.on('init', (data) => {
+        // this.socket.emit('ready');
+        this.socket.on('readyOK', (data) => {
+          this.allCard = data;
+          this.socket.emit('getCard');
+        });
+
+        this.socket.on('getPlayerId', (id) => {
+          this.playerId = id;
+        });
+
+        this.socket.on('lockOperator', (id) => {
+          this.yourturn = false;
+          this.needUseCard = false;
+          this.needSetPerson = false;
+        });
+
+        this.socket.on('unlockOperator', (id) => {
+          this.yourturn = true;
+        });
+
+        this.socket.on('getCardList', (data) => {
+          this.cardList = data.cardInHand;
+          this.hp = data.hp;
+          this.gp = data.gp;
+          this.clearDesk = data.clearDesk;
+
+          if (this.clearDesk === '1') {
+            this.desk.forEach(()=> {
+              this.desk.pop();
+            });
+          }
+
+          switch (data.nextOperator) {
+            case 1:
+              this.stage = '抽卡阶段';
+              break;
+            case 2:
+              this.needUseCard = true;
+              this.stage = '发牌阶段';
+              break;
+            case 3:
+              this.stage = '指定人';
+              this.needSetPerson = true;
+              break;
+            case 4:
+              this.stage = '养成阶段';
+              this.needUseCard = true;
+              break;
+            case 5:
+              this.stage = '连锁阶段';
+              this.needUseCard = true;
+              break;
+            case 6:
+              this.stage = '清算阶段';
+              this.needUseCard = false;
+              this.needSetPerson = false;
+              break;
+            default:
+              this.needUseCard = false;
+              this.needSetPerson = false;
+          };
+        });
+
+        this.socket.on('start', (data) => {
           // socket.emit('my other event', { my: 'data' });
           this.uploadStage('开始游戏');
-//
-          console.log('hello');
-          this.roundCounter++;
-          if (this.roundCounter % 4 === 1) {
-
-          }
+          this.socket.emit('ready');
+          // console.log('hello');
+          // this.roundCounter++;
+          // if (this.roundCounter % 4 === 1) {
+          //
+          // }
         });
       }
     },
